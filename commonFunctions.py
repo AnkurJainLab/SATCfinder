20a7b3d3ee5b4be77f7a1f44b81f9e56a1612776
rachelprime <itt.rachel@gmail.com>
2023-08-21 16:41:29 -0400

    Rewrite trimming function (simplify, add options, add run commands to SAM header)

--- commonFunctions.py
+++ commonFunctions.py
@@ -1,3 +1,7 @@
+import logging
+logging.basicConfig(format='%(asctime)s %(levelname)s : %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.INFO)
+
+VERSION = 0.1
 
 def getReverseComplement(sequence):
     # First, create a list of the complementary bases
@@ -42,10 +46,15 @@ def parseDegenerateSequence(_sequence):
 
     _editedSequence = ""
     for _base in range(0, len(_sequence)):
-        if _sequence[_base] in _degenerateBases:
+        if _sequence[_base] in ['A', 'T', 'G', 'C', 'U']:
+            _editedSequence = _editedSequence + _sequence[_base]
+        elif _sequence[_base] in _degenerateBases:
             _editedSequence = _editedSequence + _degenerateBases[_sequence[_base]]
         else:
             _editedSequence = _editedSequence + _sequence[_base]
+
+            logging.warning(f"Base {_sequence[_base]} not in nucleotides [ATGCU]"
+                            f" or IUPAC degenerate bases [RYSWKMBDHVN]")
     _sequenceForward = _editedSequence
-    _sequenceReverse = getReverseComplement(_sequenceForward).replace('[', '~').replace(']', '[').replace('~', ']')
+    _sequenceReverse = getReverseComplement(_sequenceForward)
     return _sequenceForward, _sequenceReverse
--- removeRepeatsAndConvertFASTQToSAM.py
+++ removeRepeatsAndConvertFASTQToSAM.py
@@ -7,56 +7,51 @@ Created on Wed Aug 19 23:44:55 2020
 """
 import logging
 logging.basicConfig(format='%(asctime)s %(levelname)s : %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.INFO)
-
+import commonFunctions as CF
 import argparse
 import copy
 import gzip
 import re
-import sys
 import time
 from itertools import zip_longest
 import numpy as np
-import rachelcore as rc
 
 
+def main():
 
-def main(argv):
     parser = argparse.ArgumentParser(description='Mate-aware trimming of repeats from reads')
-    parser.add_argument('--in1', type=str, default="", required=True,
-                        help='read1 file (gzipped)')
-    parser.add_argument('--in2', type=str, default="", required=False,
-                        help='read2 file (gzipped)')
-    parser.add_argument('--out', type=str, default="", required=True,
-                        help='output file')
-    parser.add_argument('--sra', type=str, default="", required=True,
-                        help='SRA of the study, to be embedded in the SAM file')
-    parser.add_argument('--disease', type=str, default="", required=True,
-                        help='Disease associated gene or CTRL, to be embedded in the SAM file')
-    parser.add_argument('--minRepeats', type=int, default="4", required=True,
+    parser.add_argument('--inFASTQ1', type=str, default="", required=True,
+                        help='path to read1 fastq file (gzipped)')
+    parser.add_argument('--inFASTQ2', type=str, default="", required=False,
+                        help='path to read2 fastq file (gzipped)')
+    parser.add_argument('--outSAM', type=str, default="", required=True,
+                        help='path to output SAM file (gzipped)')
+    parser.add_argument('--note', type=str, default="", required=False,
+                        help='Optional note (e.g. dataset ID) to save in read tags. Will be saved with every read, so'
+                             'Should be short and only alphanumeric characters.')
+    parser.add_argument('--minRepeats', type=int, default=3, required=True,
                         help='Minimum # of repeats to strip from reads')
     parser.add_argument('--UMIlength', type=int, default=0, required=False,
                         help='Length of UMI (barcode) present, assumed to be first bases in read1')
+    parser.add_argument('--minReadLengthAfterTrim', type=int, default=15, required=False,
+                        help='Minimum length of read after trimming needed to output')
     parser.add_argument('--repeatSequence', type=str, default="CAG", required=True,
                         help='Forward repeat to strip from reads. Reverse complement is also searched. '
                              'Degenerate IUPAC bases [RYSWKMBDHVN] are accepted.')
-    parser.add_argument('--log', type=str, default="", required=False,
-                        help='File to save log information')
-    parser.add_argument('--keepAllReads', action='store_true')
+    parser.add_argument('--outLog', type=str, default="", required=False,
+                        help='path to save log information file')
+    parser.add_argument('--keepAllReads', action='store_true', help='If present, output all reads to SAM file,'
+                                                                    'even those without repeats.')
 
     args = parser.parse_args()
 
     removeRepeatsAndConvertFASTQToSAM(args)
 
 
-# args.sra, args.disease, args.in1, args.in2, args.out, args.minRepeats, args.UMIlength,
-# args.repeatSequence, args.log, args.keepAllReads)
-
-
 def removeRepeatsAndConvertFASTQToSAM(_args):
-    # _SRA, _disease, _filePathIn1, _filePathIn2, _filePathOut, _minRepeats, _UMIlength,
-    # _args.repeatForward, _logPath, _keepAllReads):
+    logging.info(f"Starting run with options {_args}")
     _tStart = time.time()
-
+    # Structure for every read:
     _readBlank = {'read': "",
                   'phred': "",
                   'ID': "",
@@ -71,47 +66,35 @@ def removeRepeatsAndConvertFASTQToSAM(_args):
     _read2 = copy.deepcopy(_readBlank)  # In case read2 is not provided, initialize as blank for calculations later
     _repeatDistribution = np.zeros(100)
     _i = 0
-    _n = 1
+    _numReads = 0
     _data = {}
-    _short = 0
-    _long = 0
-    # Build the repeats, allowing for degenerate IUPAC bases ## TODO replace with commonFunctions
-    _degenerateBases = {'R': '[AG]',
-                        'Y': '[CT]',
-                        'S': '[GC]',
-                        'W': '[AT]',
-                        'K': '[GT]',
-                        'M': '[AC]',
-                        'B': '[CGT]',
-                        'D': '[AGT]',
-                        'H': '[ACT]',
-                        'V': '[ACG]',
-                        'N': '[ATGC]'}
-
-    _editedRepeat = ""
-    for _base in range(0, len(_args.repeatForward)):
-        if _args.repeatForward[_base] in _degenerateBases:
-            _editedRepeat = _editedRepeat + _degenerateBases[_args.repeatForward[_base]]
-        else:
-            _editedRepeat = _editedRepeat + _args.repeatForward[_base]
-    _repeatForward = _editedRepeat
-    _repeatReverse = rc.getReverseComplement(_repeatForward).replace('[', '~').replace(']', '[').replace('~', ']')
+    _readOutcomes = {'tooShort': 0, 'noRepeat': 0, 'good': 0}
+    _position = 0
+    # Build the repeats, allowing for degenerate IUPAC bases
+    (_repeatForward, _repeatReverse) = CF.parseDegenerateSequence(_args.repeatSequence)
     _findFwdRepeat = re.compile(r'(' + _repeatForward + '){' + str(_args.minRepeats) + ',}')
     _findRevRepeat = re.compile(r'(' + _repeatReverse + '){' + str(_args.minRepeats) + ',}')
 
-    _position = 0
-
     # Open both input files (paired end) and create the output sam file
     # using popen because it is slightly faster than built-in gzip. Not a lot, but..
-    _fileOut = gzip.open(_args.filePathOut, 'wt', compresslevel=2)
-    _fileOut.write("@HD\tVN:1.6\tSO:queryname\n")  # Write SAM header
-    _fileIn1 = gzip.open(_args.filePathIn1)
-    if _args.filePathIn2:
-        _fileIn2 = gzip.open(_args.filePathIn2)
+    _fileOut = gzip.open(_args.outSAM, 'wt', compresslevel=2)
+    # Write SAM header
+    _fileOut.write("@HD\t"
+                   "VN:1.6\tSO:queryname\n")
+    _fileOut.write("@PG\t"
+                   "ID:SATCfinder\t"
+                   "PN:SATCfinder\t"
+                   f"VN:{CF.VERSION}\t"
+                   "CL:removeRepeatsAndConvertFASTQToSAM " +
+                   "\t".join([f"--{key}\t{value}" for key, value in vars(_args).items()]) + "\n")
+
+    _fileIn1 = gzip.open(_args.inFASTQ1, "rt")
+    if _args.inFASTQ2:
+        _fileIn2 = gzip.open(_args.inFASTQ2, "rt")
     else:
         # This is a hack to allow us to iterate over either 1 or 2 files with the same code
         _fileIn2 = [None]
-    for _in1, _in2 in zip_longest(_fileIn1, _fileIn2):
+    for _lineIn1, _lineIn2 in zip_longest(_fileIn1, _fileIn2):
         # Expected format is roughly:
         # @SRR8393695.1 1/1
         # CTGGCGGCCGCGGGGACCAGCCGCGCTTTCAGCAGCACCACGGCCAGGCCGAGAAGCAGGGTGCAGGGGACACGCCGGCAGAGCCTCGCCATGGCCTAGAG
@@ -127,18 +110,18 @@ def removeRepeatsAndConvertFASTQToSAM(_args):
         # Repeat.
         if _position == 0:
             _read1 = copy.deepcopy(_readBlank)
-            _read1['ID'] = _in1.strip().replace('@', '').replace(' ', '_')
+            _read1['ID'] = _lineIn1.strip().replace('@', '').replace(' ', '_')  # Handle spaces in names
 
-            if _in2:
+            if _lineIn2:
                 _read2 = copy.deepcopy(_readBlank)
-                _read2['ID'] = _in2.strip().replace('@', '').replace(' ', '_')
+                _read2['ID'] = _lineIn2.strip().replace('@', '').replace(' ', '_')
             _position += 1
             continue
 
         if _position == 1:
-            _read1['read'] = _in1.strip()
-            if _in2:
-                _read2['read'] = _in2.strip()
+            _read1['read'] = _lineIn1.strip()
+            if _lineIn2:
+                _read2['read'] = _lineIn2.strip()
             _position += 1
             continue
 
@@ -147,19 +130,20 @@ def removeRepeatsAndConvertFASTQToSAM(_args):
             continue
 
         if _position == 3:
-            _read1['phred'] = _in1.strip()
-            if _in2:
-                _read2['phred'] = _in2.strip()
+            _read1['phred'] = _lineIn1.strip()
+            if _lineIn2:
+                _read2['phred'] = _lineIn2.strip()
             _position = 0
-            _n += 1
+            _numReads += 1
 
-        if _n % 1000000 == 0:
-            logging.info(f"{_n // 1000000}M reads trimmed in {round(time.time() - _tStart, 2)} seconds")
+        # Let's not store too much in memory
+        if _numReads % 1000000 == 0:
+            logging.info(f"{_numReads // 1000000}M reads trimmed in {round(time.time() - _tStart, 0)} seconds")
             writeRecordsToDisk(_fileOut, _data, _args)
             del _data
             _data = {}
-        # Sometimes it fails silently (with empty lines) rather than stopping at end of file.
-        # Not sure why. Hack to avoid this
+        # Sometimes open fails silently (with empty lines) rather than stopping at end of file.
+        # Check for empty line and abort if found
         if _read1['ID'] == "":
             break
 
@@ -172,55 +156,35 @@ def removeRepeatsAndConvertFASTQToSAM(_args):
 
         # Assume we throw out reads, only keep if we find a repeat of sufficient length
         _hit = False
-
-        _fwdRepeatFile1 = re.search(_findFwdRepeat, _read1['read'])
-        if _fwdRepeatFile1 is not None:
-            _hit = True
-            _start = _fwdRepeatFile1.start(0)
-            _read1['repeatFwdSequence'] = _read1['read'][_start:]
-            _read1['repeatFwdPhred'] = _read1['phred'][_start:]
-            _read1['repeatFwdLength'] = (_fwdRepeatFile1.end(0) - _start) // 3
-            _read1['read'] = _read1['read'][:_start]
-            _read1['phred'] = _read1['phred'][:_start]
-
-        _revRepeatFile1 = re.finditer(_findRevRepeat, _read1['read'])
-        _result = None
-        for _result in _revRepeatFile1:
-            pass
-
-        if _result is not None:
-            _hit = True
-            _end = _result.end(0)
-            _read1['repeatRevSequence'] = _read1['read'][:_end]
-            _read1['repeatRevPhred'] = _read1['phred'][:_end]
-            _read1['repeatRevLength'] = (_end - _result.start(0)) // 3
-            _read1['read'] = _read1['read'][_end:]
-            _read1['phred'] = _read1['phred'][_end:]
-
-        if _in2:
-            _fwdRepeatFile2 = re.search(_findFwdRepeat, _read2['read'])
-            if _fwdRepeatFile2 is not None:
+        _readNum = 1
+        for _whichRead in [_read1, _read2]:
+            if (_readNum == 2) and not _lineIn2:
+                continue
+            _fwdRepeatMatches = re.search(_findFwdRepeat, _whichRead['read'])
+            if _fwdRepeatMatches is not None:
                 _hit = True
-                _start = _fwdRepeatFile2.start(0)
-                _read2['repeatFwdSequence'] = _read2['read'][_start:]
-                _read2['repeatFwdPhred'] = _read2['phred'][_start:]
-                _read2['repeatFwdLength'] = (_fwdRepeatFile2.end(0) - _start) // 3
-                _read2['read'] = _read2['read'][:_start]
-                _read2['phred'] = _read2['phred'][:_start]
+                _start = _fwdRepeatMatches.start(0)
+                _whichRead['repeatFwdSequence'] = _whichRead['read'][_start:]
+                _whichRead['repeatFwdPhred'] = _whichRead['phred'][_start:]
+                _whichRead['repeatFwdLength'] = (_fwdRepeatMatches.end(0) - _start) // 3
+                _whichRead['read'] = _whichRead['read'][:_start]
+                _whichRead['phred'] = _whichRead['phred'][:_start]
 
-            _revRepeatFile2 = re.finditer(_findRevRepeat, _read2['read'])
+            # Skip to last instance of repeat in read. Fix for interrupted repeats
+            _revRepeatMatches = re.finditer(_findRevRepeat, _whichRead['read'])
             _result = None
-            for _result in _revRepeatFile2:
+            for _result in _revRepeatMatches:
                 pass
 
             if _result is not None:
                 _hit = True
                 _end = _result.end(0)
-                _read2['repeatRevSequence'] = _read2['read'][:_end]
-                _read2['repeatRevPhred'] = _read2['phred'][:_end]
-                _read2['repeatRevLength'] = (_end - _result.start(0)) // 3
-                _read2['read'] = _read2['read'][_end:]
-                _read2['phred'] = _read2['phred'][_end:]
+                _whichRead['repeatRevSequence'] = _whichRead['read'][:_end]
+                _whichRead['repeatRevPhred'] = _whichRead['phred'][:_end]
+                _whichRead['repeatRevLength'] = (_end - _result.start(0)) // 3
+                _whichRead['read'] = _whichRead['read'][_end:]
+                _whichRead['phred'] = _whichRead['phred'][_end:]
+            _readNum = 2
 
         # Save repeat length distribution (optional statistics)
         _repeatDistribution[max([_read1['repeatRevLength'], _read1['repeatFwdLength'], _read2['repeatRevLength'],
@@ -229,27 +193,29 @@ def removeRepeatsAndConvertFASTQToSAM(_args):
         # if we got a match, save the read(s)
         if _hit | _args.keepAllReads:
             if len(_read1['read']) != len(_read1['phred']):
-                rc.msg("Fatal error: Mismatched lengths!", _read1)
+                logging.error("Fatal error: Read has mismatched sequence and phred lengths", _read1)
                 break
-            if _in2 and (len(_read2['read']) != len(_read2['phred'])):
-                rc.msg("Fatal error: Mismatched lengths!", _read2)
+            if _lineIn2 and (len(_read2['read']) != len(_read2['phred'])):
+                logging.error("Fatal error: Read has mismatched sequence and phred lengths", _read2)
                 break
-            # 0-length reads kill STAR. Also, short reads aren't mappable... so let's omit anything shorter than 8 bases
-            if not _in2:
-                if len(_read1['read']) >= 8:
+            # 0-length reads kill STAR. Also, short reads aren't mappable
+            if not _lineIn2:
+                if len(_read1['read']) >= _args.minReadLengthAfterTrim:
                     _data[_i] = _read1
                     _i += 1
-                    _long += 1
+                    _readOutcomes['good'] += 1
                 else:
-                    _short += 1
-            elif len(_read1['read']) >= 8 and len(_read2['read']) >= 8:
+                    _readOutcomes['tooShort'] += 1
+            elif len(_read1['read']) >= _args.minReadLengthAfterTrim and \
+                    len(_read2['read']) >= _args.minReadLengthAfterTrim:
                 _data[_i] = _read1
                 _data[_i + 1] = _read2
                 _i += 2
-                _long += 1
+                _readOutcomes['good'] += 1
             else:
-                _short += 1
+                _readOutcomes['tooShort'] += 1
         else:
+            _readOutcomes['noRepeat'] += 1
             # We didn't find anything. Continue!
             continue
 
@@ -259,19 +225,23 @@ def removeRepeatsAndConvertFASTQToSAM(_args):
 
     _tEnd = time.time()
     _tDiff = _tEnd - _tStart
-    logging.info(f"Processed {str(_n)} reads in {str(round(_tDiff, 2))} seconds ({_n / _tDiff // 1}/sec)")
-    logging.info(f"{_long} reads ({_long / _n * 100}%) had repeats and were successfully trimmed "
-                 f"and converted to SAM format")
-    logging.info(f"{_short} reads ({_short / _n * 100}%) had repeats, were discarded because the non-repetitive region "
+    logging.info(f"Processed {str(_numReads)} reads in {round(_tDiff, 2)} seconds ({_numReads / _tDiff // 1}/sec)")
+    logging.info(f"{_readOutcomes['good']} reads ({round(_readOutcomes['good'] / _numReads * 100,2)}%) "
+                 f"had repeats and were successfully trimmed and converted to SAM format")
+    logging.info(f"{_readOutcomes['tooShort']} reads ({round(_readOutcomes['tooShort'] / _numReads * 100, 2)}%)"
+                 f" had repeats, but were discarded because the non-repetitive region "
                  f"was shorter than --minReadLength")
-    logging.info(_repeatDistribution)
-    if _args.logPath != "":
-        with open(_args.logPath, "wt") as _logFileOut:
+    logging.info(f"{_readOutcomes['noRepeat']} reads ({round(_readOutcomes['noRepeat'] / _numReads * 100, 2)}%)"
+                 f" had no repeats matching >={_args.minRepeats}x{_repeatForward}/{_repeatReverse}")
+
+    if _args.outLog != "":
+        with open(_args.outLog, "wt") as _logFileOut:
+            _logFileOut.write(f"Run time (s):,{_tDiff // 1}\n")
+            _logFileOut.write(f"Total reads:,{_numReads}\n")
+            _logFileOut.write(f"Reads too short,{_readOutcomes['tooShort']}\n")
+            _logFileOut.write(f"Reads without repeats,{_readOutcomes['noRepeat']}\n")
             for _i in range(0, len(_repeatDistribution)):
-                _logFileOut.write("{},{}\n".format(_i, _repeatDistribution[_i]))
-            _logFileOut.write("Run time (s):,{}\n".format(_tDiff // 1))
-            _logFileOut.write("Total reads:,{}\n".format(_n))
-            _logFileOut.write("Reads too short,{}\n".format(_short))
+                _logFileOut.write(f"{_i},{_repeatDistribution[_i]}\n")
 
 
 def writeRecordsToDisk(_fileOut, _data, _args):
@@ -293,8 +263,7 @@ def writeRecordsToDisk(_fileOut, _data, _args):
                    'tS:Z:' + _line['repeatRevSequence'],
                    'tL:i:' + str(_line['repeatRevLength']),
                    'tP:Z:' + _line['repeatRevPhred'],
-                   'rS:Z:' + _args.SRA,
-                   'rD:Z:' + _args.disease,
+                   'rS:Z:' + _args.note,
                    'RX:Z:' + _line['UMI'],
                    'pX:Z:' + _line['UMIphred']]
 
@@ -302,4 +271,4 @@ def writeRecordsToDisk(_fileOut, _data, _args):
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
